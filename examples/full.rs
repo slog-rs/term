2e38af6f8c28e331b7d139c4c0777c1356d3f514
Dawid Ciężarkiewicz <dpc@dpc.pw>
2017-05-05 21:23:42 -0700

    Remove unnecessary `Arc` from examples

--- examples/full.rs
+++ examples/full.rs
@@ -3,7 +3,6 @@ extern crate slog;
 extern crate slog_term;
 
 use slog::Drain;
-use std::sync::Arc;
 
 mod common;
 
@@ -11,7 +10,7 @@ fn main() {
     let decorator = slog_term::PlainSyncDecorator::new(std::io::stdout());
     let drain = slog_term::FullFormat::new(decorator).build().fuse();
 
-    let log = slog::Logger::root(Arc::new(drain), o!("version" => "0.5"));
+    let log = slog::Logger::root(drain, o!("version" => "0.5"));
 
     common::simulate_server(log);
 }
